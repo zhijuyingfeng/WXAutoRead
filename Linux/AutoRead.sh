6839197e99cda159c5d2faec36a132b8ee75e1c7
zhijuyongfeng <2604385867@qq.com>
2019-05-20 10:25:24 +0800

    random time to avoid cheating detection

--- Linux/AutoRead.sh
+++ Linux/AutoRead.sh
@@ -4,11 +4,13 @@ adb devices -l
 test $? -ne 0 && echo "No android device!" &&exit
 
 total=0
+ran=0
 while [ $total -le $1 ]
 do
     adb shell input tap 1000 200
     test $? -ne 0 && exit
-    sleep 5
+    ran=$(($RANDOM%10+10))
+    sleep $ran
     total=$(($total+8))
 done
 echo "Finished!"
